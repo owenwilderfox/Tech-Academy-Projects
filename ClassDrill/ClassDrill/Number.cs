00c2afc42dd03c62a61a2a0f9fa1a59a1c487381
Savannah Fox <savannahfox@gmail.com>
2019-01-02 12:05:38 -0800

    Initial commit of Class Drill. Create of program and class for Number.

---
+++ ClassDrill/ClassDrill/Number.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDrill
+{
+    public class Number
+    {
+        public Number()
+        {
+            Value = 1;
+        }
+        public int Value { get; set; }
+
+    }
+}
---
+++ ClassDrill/ClassDrill/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Please enter an interger you would like to work with:");
+            int enteredNumber = Convert.ToInt32(Console.ReadLine());
+
+            Number number = new Number();
+            number.Value = enteredNumber;
+
+            Console.WriteLine("The number you are working with today is " + number.Value + ".");
+            Console.ReadLine();
+
+        }
+    }
+}
