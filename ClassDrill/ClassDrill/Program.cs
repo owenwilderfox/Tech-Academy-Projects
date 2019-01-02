e81f66b0c6fd9865473425d7c279581b1e548b3c
Savannah Fox <savannahfox@gmail.com>
2019-01-02 12:34:52 -0800

    Create three methods for processing user input.

--- ClassDrill/ClassDrill/Program.cs
+++ ClassDrill/ClassDrill/Program.cs
@@ -16,10 +16,38 @@ namespace ClassDrill
 
             Number number = new Number();
             number.Value = enteredNumber;
+            number = Add(number);
+            number = Divide(number);
+            number = Multiply(number);
 
-            Console.WriteLine("The number you are working with today is " + number.Value + ".");
+            Console.WriteLine("After processing, your number is now " + number.Value + ".");
             Console.ReadLine();
 
+        }
+
+        public static Number Add(Number number)
+        {
+
+            int tempAddnumber = number.Value + 10;
+            number.Value = tempAddnumber;
+            return number;
+
+        }
+
+        public static Number Divide(Number number)
+        {
+
+            int tempDividenumber = number.Value / 2;
+            number.Value = tempDividenumber;
+            return number;
+
+        }
+
+        public static Number Multiply(Number number)
+        {
+            int tempMultiplynumber = number.Value * 20;
+            number.Value = tempMultiplynumber;
+            return number;
         }
     }
 }
